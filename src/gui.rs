31a1ce75459ba3283dccfa1b0aad15509f891431
Ionut <barborionut15@gmail.com>
2024-07-28 22:25:12 +0300

    gui setup and some experiments

---
+++ src/gui.rs
@@ -0,0 +1,5 @@
+use bevy_egui::{egui, EguiContexts, EguiPlugin};
+
+pub fn UI(mut contexts: EguiContexts) {
+    //make UI bar down there
+}
--- src/main.rs
+++ src/main.rs
@@ -1,21 +1,25 @@
 use bevy::prelude::*;
+use bevy_egui::{egui, EguiContexts, EguiPlugin};
 
 mod camera;
 mod canvas;
+mod gui;
 
 use camera::camera_movement_system;
 use camera::spawn_camera;
 use canvas::spawn_center;
 use canvas::spawn_light;
 use canvas::spawn_plane;
+use gui::UI;
 
 fn main() {
     App::new()
         .add_plugins(DefaultPlugins)
+        .add_plugins(EguiPlugin)
         .add_systems(
             Startup,
             (spawn_camera, spawn_plane, spawn_center, spawn_light),
         )
-        .add_systems(Update, camera_movement_system)
+        .add_systems(Update, (camera_movement_system, UI))
         .run();
 }
